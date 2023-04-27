4c232ee10d5fe1ccf969eeb9fa0e6560a89a734a
titan-of-infinite-void <bhaweshsoni29@gmail.com>
2023-04-27 14:32:22 +0530

    Added Firebase

---
+++ src/db.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWkzAhkRe5NtVbXHbaQTO5u1-5zPWKDRo",
+  authDomain: "studyguru-7.firebaseapp.com",
+  projectId: "studyguru-7",
+  storageBucket: "studyguru-7.appspot.com",
+  messagingSenderId: "607540387981",
+  appId: "1:607540387981:web:821e5064b39907b03a42c4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
